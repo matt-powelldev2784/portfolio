84f92b7a0936c4de5563a47f10c39a2a632c3242
Matthew Powell <matt.powell2784@gmail.com>
2022-12-30 17:20:29 +0000

    navigation initial commit

--- js/main.js
+++ js/main.js
@@ -2,3 +2,20 @@ setInterval(() => {
   console.log('window.innerWidth', window.innerWidth)
   console.log('window.innerHeight', window.innerHeight)
 }, 3000)
+
+const homepage = document.getElementsByClassName('homepage__section')[0]
+const about = document.getElementsByClassName('about__section')[0]
+const projects = document.getElementsByClassName('projects__section')[0]
+const contact = document.getElementsByClassName('contact__section')[0]
+const nav = document.getElementsByClassName('main_nav__links_flexbox')[0]
+console.log('about', about)
+
+const body = document.getElementById('body')
+console.log('body', body)
+const pages = [about, projects, contact]
+
+pages.forEach((page) => {
+  page.remove()
+})
+homepage.remove()
+nav.after(projects)
