b8c2a30a54b9b27349c32ee7e1fec405fa932b71
Matthew Powell <matt.powell2784@gmail.com>
2022-12-30 19:45:03 +0000

    scroll test

--- js/main.js
+++ js/main.js
@@ -8,17 +8,5 @@ const about = document.getElementsByClassName('about__section')[0]
 const projects = document.getElementsByClassName('projects__section')[0]
 const contact = document.getElementsByClassName('contact__section')[0]
 const nav = document.getElementsByClassName('main_nav__links_flexbox')[0]
-console.log('about', about)
 
-const body = document.getElementById('body')
-console.log('body', body)
-const pages = [about, projects, contact]
-
-pages.forEach((page) => {})
-
-const test = projects.getBoundingClientRect()
-console.log('test', test)
-
-// window.scroll(0, test.y + 35)
-
-projects.scrollIntoView({ block: 'start', behavior: 'smooth', inline: 'nearest' })
+contact.scrollIntoView(true, { block: 'start', behavior: 'smooth', inline: 'nearest' })
