30fc36775ecb9443efddc72295d7a2912cd6ca97
Matthew Powell <matt.powell2784@gmail.com>
2022-12-31 17:51:01 +0000

    projects carousel mobile nav arrows added

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
-import { loadNavigationEventListners } from './eventListeners/navigation.js'
-import { toggleThemeEventListeners } from './eventListeners/toggleTheme.js'
+import { loadNavigationEventListners } from './eventListeners/navigation.js';
+import { toggleThemeEventListeners } from './eventListeners/toggleTheme.js';
 
-// setInterval(() => {
-//   console.log('window.innerWidth', window.innerWidth)
-//   console.log('window.innerHeight', window.innerHeight)
-// }, 3000)
+setInterval(() => {
+  console.log('window.innerWidth', window.innerWidth);
+  console.log('window.innerHeight', window.innerHeight);
+}, 3000);
 
 const loadEventListeners = () => {
-  loadNavigationEventListners()
-  toggleThemeEventListeners()
-}
-loadEventListeners()
+  loadNavigationEventListners();
+  toggleThemeEventListeners();
+};
+loadEventListeners();
