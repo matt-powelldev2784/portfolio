54d73a444858693306276eab920c90e9a33fab80
Matthew Powell <matt.powell2784@gmail.com>
2022-12-31 15:53:20 +0000

    contact nav href and event listener added

--- js/eventListeners/toggleTheme.js
+++ js/eventListeners/toggleTheme.js
@@ -1,6 +1,9 @@
 import { toogleTheme } from '../theme/toggleTheme.js'
 
 export const toggleThemeEventListeners = () => {
+  const lightElement = document.getElementById('contact_nav__theme_toggle')
+  lightElement.addEventListener('click', toogleTheme)
+
   document.addEventListener('keydown', (event) => {
     if (event.key === 'l') {
       toogleTheme()
