9660ec37ccd5c803850fbf9d13de56a791da0d39
Matthew Powell <matt.powell2784@gmail.com>
2023-01-01 15:09:52 +0000

    slide testing

--- js/main.js
+++ js/main.js
@@ -11,7 +11,7 @@ setInterval(() => {
 
 const loadEventListeners = () => {
   loadNavigationEventListners();
-  toggleThemeEventListeners();
+  //toggleThemeEventListeners();
   //loadProjectsEventListeners();
 };
 loadEventListeners();
