9eb40abcc7f6be8934a6f24c0df5f6c6e551fe2c
Matthew Powell <matt.powell2784@gmail.com>
2023-01-01 15:16:16 +0000

    slide testing

--- js/main.js
+++ js/main.js
@@ -3,11 +3,27 @@ import { toggleThemeEventListeners } from './eventListeners/toggleTheme.js';
 import { loadProjectsEventListeners } from './eventListeners/projects.js';
 import { loadCarouselSlide } from './projects/loadCarouselSlide.js';
 import { currentSlide } from './projects/loadCarouselSlide.js';
+import { createElement } from './helpers/createElement.js';
 
 setInterval(() => {
   console.log('window.innerWidth', window.innerWidth);
   console.log('window.innerHeight', window.innerHeight);
 }, 3000);
+
+loadCarouselSlide(currentSlide.currentSlideNumber);
+const test = document.getElementById('homepage__section');
+const test1 = createElement(
+  'div',
+  { class: 'homepage_text__h1' },
+  currentSlide.currentSlideNumber,
+  currentSlide.currentSlideNumber,
+  currentSlide.currentSlideNumber,
+  currentSlide.currentSlideNumber,
+  currentSlide.currentSlideNumber,
+  currentSlide.currentSlideNumber,
+  currentSlide.currentSlideNumber
+);
+test.append(test1);
 
 const loadEventListeners = () => {
   loadNavigationEventListners();
@@ -15,5 +31,3 @@ const loadEventListeners = () => {
   //loadProjectsEventListeners();
 };
 loadEventListeners();
-
-// loadCarouselSlide(currentSlide.currentSlideNumber);
