f0aac9bb60a700192a7871b7c75f6eb5553f3327
Matthew Powell <matt.powell2784@gmail.com>
2023-01-01 14:44:56 +0000

    slide testing

--- js/eventListeners/projects.js
+++ js/eventListeners/projects.js
@@ -3,8 +3,8 @@ import { prevSlide } from '../projects/prevslide.js';
 
 export const loadProjectsEventListeners = () => {
   const nextSlideArrow = document.getElementById('projects__nav_arrow_next');
-  nextSlideArrow.addEventListener('click', nextSlide);
+  nextSlideArrow && nextSlideArrow.addEventListener('click', nextSlide);
 
   const prevSlideArrow = document.getElementById('projects__nav_arrow_prev');
-  prevSlideArrow.addEventListener('click', prevSlide);
+  prevSlideArrow && prevSlideArrow.addEventListener('click', prevSlide);
 };
