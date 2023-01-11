47f7e2c3bd0bcd3cd676a6c2b0e2ebfe03dbdb7e
Matthew Powell <matt.powell2784@gmail.com>
2023-01-11 21:00:47 +0000

    projects text update

--- js/projects/projects.js
+++ js/projects/projects.js
@@ -5,9 +5,8 @@ export const projects = [
     text: `Showcase website project built using HTML, JavaScript, and CSS. Flexbox, grid and media queries are utilised 
     to provide a fully responsive layout. A light and dark theme can be toggled using the menu. SVG sprites are used
     so the icons also change colour when the light or dark theme is toggled. I have used a carousel to display the
-    projects. I have created touch detection logic so the carousel can be swiped on touch devices. I have used an
-    intersection observer to detect when the projects section comes into view so I can toggle the display of a swipe
-    icon to alert touch users of the swipe functionality.`,
+    projects. I have created touch detection logic so the carousel can be swiped on touch devices and used an
+    intersection observer to toggle the display of a swipe icon to alert touch users of the swipe functionality.`,
     techImages: [
       { src: './img/svg_sprites/html_icon.svg', alt: 'HTML Icon' },
       { src: './img/svg_sprites/js_icon.svg', alt: 'JavaScript Icon' },
