207ad7ce470dfdbf9cd9ab221b969f5ae94f16e0
Matthew Powell <matt.powell2784@gmail.com>
2025-02-04 18:54:33 +0000

    feat: update projects wording

--- js/projects/projects.js
+++ js/projects/projects.js
@@ -4,7 +4,7 @@ export const projects = [
     title: 'NEXTJS TAKEAWAY APP',
     text: `A food takeaway application built to explore the main features of NextJS.
           Built with NextJS, Sanity CMS, TypeScript, Postgres, Prisma and TailwindCSS.
-          The site utilises NextJS features for SSR, ISR, API creation, dynamic routes, and image optimization.
+          The site utilises NextJS features for SSR, ISR, API creation, dynamic routes, and image optimisation.
           Menu items can be added, edited and deleted using the Sanity CMS.
           User authentication implemented with Next-Auth. Payment gateway integrated using Stripe.`,
     techImages: [
